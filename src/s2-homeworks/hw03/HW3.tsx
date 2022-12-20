382028000fb7503b19e83877b5f2a98b31b87be7
Nagima <nagikh03@gmail.com>
2022-12-20 13:58:27 +0300

    hw3 inputClass fix

--- src/s2-homeworks/hw03/HW3.tsx
+++ src/s2-homeworks/hw03/HW3.tsx
@@ -23,11 +23,13 @@ export type UserType = {
     name: string // need to fix any
 }
 
-export const pureAddUserCallback = (name: string, setUsers:  (items: any[])=> void, users: Array<UserType>,) => { // need to fix any
+export const pureAddUserCallback = (name: string, setUsers: (value: Array<UserType>) => void, users: Array<UserType>,) => { // need to fix any
     const user =  { _id: v1(), name: name }// need to fix
 
     setUsers([...users, user])
 }
+
+
 
 const HW3 = () => {
     const [users, setUsers] = useState<Array<UserType>>([]) // need to fix any
