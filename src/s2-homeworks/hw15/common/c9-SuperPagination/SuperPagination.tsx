f2a878199b1bf6d5afdde74f0fe951f800d2a65a
Nagima <nagikh03@gmail.com>
2023-04-11 21:06:17 +0300

    hw15 comm

--- src/s2-homeworks/hw15/common/c9-SuperPagination/SuperPagination.tsx
+++ src/s2-homeworks/hw15/common/c9-SuperPagination/SuperPagination.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {ChangeEvent} from 'react'
 import SuperSelect from '../../../hw07/common/c5-SuperSelect/SuperSelect'
 import {Pagination} from '@mui/material'
 import s from './SuperPagination.module.css'
@@ -16,43 +16,46 @@ const SuperPagination: React.FC<SuperPaginationPropsType> = (
         page, itemsCountForPage, totalCount, onChange, id = 'hw15',
     }
 ) => {
-    const lastPage = 10 // пишет студент // вычислить количество страниц
-
+    //const lastPage = 10 // пишет студент // вычислить количество страниц
+    const lastPage = Math.ceil(totalCount/itemsCountForPage) // пишет студент // вычислить количество страниц
     const onChangeCallback = (event: any, page: number) => {
         // пишет студент
+        const num = event.currentTarget.value ? +event.currentTarget.value : itemsCountForPage
+        onChange(page,num )
     }
 
-    const onChangeSelect = (event: any) => {
+    const onChangeSelect = (event: ChangeEvent<HTMLSelectElement>) => {//event: ChangeEvent<HTMLSelectElement>
         // пишет студент
+        onChangeCallback(event, page)
     }
 
     return (
         <div className={s.pagination}>
-            <Pagination
-                id={id + '-pagination'}
-                sx={{
-                    // стили для Pagination // пишет студент
-                }}
-                page={page}
-                count={lastPage}
-                onChange={onChangeCallback}
-                hideNextButton
-                hidePrevButton
+            <Pagination shape="rounded" color="primary"
+                        id={id + '-pagination'}
+                        sx={{
+                            // стили для Pagination // пишет студент
+                        }}
+                        page={page}
+                        count={lastPage}
+                        onChange={onChangeCallback}
+                        hideNextButton
+                        hidePrevButton
             />
 
             <span className={s.text1}>
                 показать
             </span>
 
-            <SuperSelect
-                id={id + '-pagination-select'}
-                value={itemsCountForPage}
-                options={[
-                    {id: 4, value: 4},
-                    {id: 7, value: 7},
-                    {id: 10, value: 10},
-                ]}
-                onChange={onChangeSelect}
+            <SuperSelect style={{width: 28, height: 24}}
+                         id={id + '-pagination-select'}
+                         value={itemsCountForPage}
+                         options={[
+                             {id: 4, value: 4},
+                             {id: 7, value: 7},
+                             {id: 10, value: 10},
+                         ]}
+                         onChange={onChangeSelect}
             />
 
             <span className={s.text2}>
